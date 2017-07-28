6ac29441f4d718bf2e59d1b20d6d93ea12ffb2d8
Avinash-Nishad-sa-oo8 <avinash.nishad.sa@gmail.com>
2017-07-28 16:51:39 +0530

    videobrowser

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class SearchBar extends Component {
   constructor(props) {
     super(props);
-
+ console.log('videoBrowser');
     this.state = { term: "" };
   }
 
